0fc385b42bc70d16650e8f83a0343eb37289f6a1
jmhar <jmhar@protonmail.com>
2021-08-07 14:52:53 +1000

    state practice

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,19 @@
 // your Bomb code here!
+import React, { Component } from 'react';
+
+export default class Bomb extends Component {
+    constructor(props) {
+        super()
+        this.state = {
+            secondsLeft: props.initialCount
+        }
+    }
+
+    render() {
+        if(this.state.secondsLeft === 0) {
+            return <h2>Boom!</h2>
+        } else {
+            return <h4>{this.state.secondsLeft} seconds left before I go boom!</h4>
+        }
+    }
+}
